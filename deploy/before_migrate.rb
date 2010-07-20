350b19a03017ba856e9834165f5fb88acdb19dda
Tung Nguyen <tongueroo@gmail.com>
2010-07-20 13:45:31 -0700

    fix ey deploy

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -19,7 +19,7 @@ end
 
 if %w(app_master app util solo).include?(node[:instance_role])
   # gems for app
-  sudo "cd #{latest_release} && rake gems:install SUDO=1 DOCS=0"
+  sudo "bundle install"
 
   # symlinks
   do_symlinks(latest_release)
