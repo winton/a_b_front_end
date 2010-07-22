1f0f37307c630ca8dbd7800f9cd4539f9016fe46
Tung Nguyen <tongueroo@gmail.com>
2010-07-22 16:12:37 -0700

    deploy: only uninstall rack 1.1.0 if its installed

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -21,7 +21,8 @@ if %w(app_master app util solo).include?(node[:instance_role])
   # gems for app
   sudo "cd #{release_path} && bundle install"
   # something is installing 2 version of rack again!, I think it's rack-flash still
-  sudo "gem uninstall rack --version=1.1.0"
+  sudo 'if gem list | grep rack | grep 1.1.0; then gem uninstall rack --version=1.1.0; else echo "dont need to uninstall rack version=1.1.0"; fi'
+
   # symlinks
   do_symlinks(latest_release)
 end
