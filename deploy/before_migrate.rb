2a3d5b3b3a7596d347ddb8513b13627dee2f813f
Tung Nguyen <tongueroo@gmail.com>
2010-07-21 17:40:12 -0700

    force uninstall rack 1.1.0

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -20,7 +20,8 @@ end
 if %w(app_master app util solo).include?(node[:instance_role])
   # gems for app
   sudo "cd #{release_path} && bundle install"
-
+  # something is installing 2 version of rack again!, I think it's rack-flash still
+  sudo "gem uninstall rack --version=1.1.0"
   # symlinks
   do_symlinks(latest_release)
 end
