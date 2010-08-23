fc78c54bc0c2187b58f2d64ac6acf804a0a2c3a8
winton <mail@wintoni.us>
2010-08-22 22:24:20 -0700

    Setting test categories in spec action instead of config yaml, preparing for fake data spec

--- lib/a_b_front_end/controller/spec.rb
+++ lib/a_b_front_end/controller/spec.rb
@@ -3,8 +3,32 @@ Application.class_eval do
   if environment == :development
     
     get '/spec/js' do
+      site = ABPlugin::Config.site
+      categories = ABPlugin::Config.categories
+      
+      ABPlugin::Config.categories [
+        {
+          :name => 'Category',
+          :tests => [
+            {
+              :id => 1,
+              :name => 'Test',
+              :variants => [
+                { :id => 2, :name => 'v1' },
+                { :id => 3, :name => 'v2' },
+                { :id => 4, :name => 'v3' }
+              ]
+            }
+          ]
+        }
+      ]
+      
       ABPlugin::API.spec_js_setup
+      
       haml :'spec/js', :layout => false
+      
+      ABPlugin::Config.site site
+      ABPlugin::Config.categories categories
     end
   end
 end
